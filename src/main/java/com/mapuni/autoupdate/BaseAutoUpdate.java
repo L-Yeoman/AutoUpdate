4ad54ded1995c942039aae788b8b4684a5a6a819
Lybin <250468162@qq.com>
2017-12-05 15:02:43 +0800

    增加7.0读写权限

--- src/main/java/com/mapuni/autoupdate/BaseAutoUpdate.java
+++ src/main/java/com/mapuni/autoupdate/BaseAutoUpdate.java
@@ -20,7 +20,8 @@ import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import android.support.v4.content.FileProvider;
+import android.os.Build;
 
 
 import org.xmlpull.v1.XmlPullParser;
@@ -209,8 +210,20 @@ public class BaseAutoUpdate {
                         /** 下载完成后调用安装程序 */
                         Intent i = new Intent(Intent.ACTION_VIEW);
                         String filePath = SDCARD_APK_LOCAL_PATH;
-                        i.setDataAndType(Uri.parse("file://" + filePath), "application/vnd.android.package-archive");
-                        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//
+                        if(Build.VERSION.SDK_INT>=24) { //判读版本是否在7.0以上
+                            File file = new File(SDCARD_APK_LOCAL_PATH);
+                            //参数1 上下文, 参数2 Provider主机地址 和配置文件中保持一致   参数3  共享的文件
+                            Uri apkUri =
+                                    FileProvider.getUriForFile(context, "com.mapuni.autoupdate.fileprovider", file);
+                            //添加这一句表示对目标应用临时授权该Uri所代表的文件
+                            i.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                            i.setDataAndType(apkUri, "application/vnd.android.package-archive");
+                        }else{
+                            i.setDataAndType(Uri.parse("file://" + filePath),
+                                    "application/vnd.android.package-archive");
+//                            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        }
                         context.startActivity(i);
                     }
                 } catch (MalformedURLException e) {
